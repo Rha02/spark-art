6fb8b5c3e07c665c7df1102778329058cd1b83fd
Rha02 <romuzus08@gmail.com>
2024-02-14 15:20:08 -0800

    Created Comment Repo

--- frontend/src/lib/types/index.ts
+++ frontend/src/lib/types/index.ts
@@ -28,3 +28,13 @@ export type Prompt = {
     responses: number;
     createdAt: Date;
 }
+
+export type ArtComment = {
+    id: number;
+    creatorId: number;
+    creatorName: string;
+    creatorIconUrl: string;
+    artworkId: number;
+    text: string;
+    createdAt: Date;
+}
