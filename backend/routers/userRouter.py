34a9d61155e7570da5a999141ad5f6967ea169b2
Rha02 <romuzus08@gmail.com>
2024-09-29 20:12:49 -0700

    HashFunc Repo

--- backend/routers/userRouter.py
+++ backend/routers/userRouter.py
@@ -1,14 +1,22 @@
 
 from fastapi import APIRouter
 
+from backend.config import get_app_config
+
 UserRouter = APIRouter()
+
+appConfig = get_app_config()
 
 @UserRouter.get("/")
 async def get_users():
     return {"message": "Get authenticated user"}
 
 @UserRouter.post("/register")
-async def register_user():
+async def register_user(
+    username: str,
+    password: str
+):
+    print(appConfig.hashrepo.hash(password))
     return {"message": "Register user"}
 
 @UserRouter.post("/login")
